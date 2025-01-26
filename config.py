45f9ced5a5bd3164681bdc53141c0e50e8958060
navneetdh <87060742+navneetdh@users.noreply.github.com>
2025-01-26 12:17:44 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Configuration for Database and API
 
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Password@123",
-    "database": "stock_market_db",
+    "host": "", # your host
+    "user": "", # your user
+    "password": "", # your password
+    "database": "",  # your database
 }
 
-API_KEY = "e69401c3a5msh774872ef30a9777p1c4ee3jsnc494f5b3840d"
+API_KEY = ""    # Your API Key
